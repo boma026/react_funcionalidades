0dc980961bbdf3e018d58076c2cce86cddbe7c1a
boma026 <arthurboma@yahoo.com.br>
2025-06-06 16:34:52 -0300

    06/05/25

--- react_funcionalidades/src/app/page.tsx
+++ react_funcionalidades/src/app/page.tsx
@@ -6,35 +6,8 @@ import { FormEvent } from "react";
 import { useState } from "react";
 
 export default function Page() {
-
-  const [showSecret, setShowSecret] = useState(false)
-
-  const botaoShowHide = () => {
-    setShowSecret(!showSecret);
-  }
-
-  const [count, setCount] = useState(0);
-
-  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) =>{
-  event.preventDefault();
-  alert("mandando....")
-}
-
-  const  eventoDeClique = () => {
-    setCount(count + 1)
-  }
-
-  const [count2, setCount2] = useState(0)
-
-  const handlebutton = () => {
-    setCount2(count2 + 2)
-    setCount2(c => c + 2)
-  }
-
-  const [nameInput, setNameInput] = useState('')
-
-  const [fullName, setFullName] = useState<Person>({ name: '', lastName: ''})
-
+  
+  //1)
    const cliqueButao = () => {
     alert("funcionou")
   }
@@ -45,6 +18,28 @@ export default function Page() {
   const clique1 = () => alert("clicou1")
   const clique2 = () => alert("clicou2")
   const clique3 = () => alert("clicou3")
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) =>{
+  event.preventDefault();
+  alert("mandando....")
+}
+  //4)
+  const[showSecret, setShowSecret] = useState(false);
+
+  const handleButtonSecret = () => {
+    setShowSecret(!showSecret)
+  }
+
+  //5)
+  const[nameInput, setNameInput] = useState<string>();
+
+  //6)
+  const [fullName, setFullName] = useState({
+    name: "",
+    lastName: ""
+  })
+
+
 
   return (
     <div>
@@ -62,29 +57,64 @@ export default function Page() {
         <CustomButton label="Clique aqui3" onClick={clique3}/>
       </ExampleAreas>
       
-      {/*
-      <h3 className="text-5xl mb-3 pt-3">Form de login</h3>
+
+      <ExampleAreas>
+      <h3 className="text-5xl mb-3 pt-3">4) Form de login</h3>
       
       <form onSubmit={handleFormSubmit}>
-        <input type="text"/>
+        <input type="text" className="bg-white"/>
         <input type="submit" value="enviar"/>
       </form>
+      </ExampleAreas>
+      
+      <ExampleAreas>
+        <div className="flex flex-col justify-center items-center">
+          {showSecret &&
+            <>
+              <CustomButton label={showSecret ? "Ocultar área secreta" : "Mostrar área secreta"} onClick={handleButtonSecret}/>
+              <div className="p-3 bg-blue-300 rounded-md mt-3">Área secreta</div>
+            </>
+          }
+        </div>
+      </ExampleAreas>
 
-      <p>{count}</p>
-      <CustomButton label="+1" onClick={eventoDeClique}/>
-
-      <CustomButton label={showSecret ? "Ocultar" : "Mostrar"} onClick={botaoShowHide}/>
-      {showSecret &&
-      <div className="p-3 bg-blue-300 rounded-md mt-3">Area secreta</div>
-      }
-
+      <ExampleAreas>    
       <input 
         type="text"
         className="border border-black p-3 text-xl text-black rounded"
         placeholder="Digite seu nome"
         value={nameInput}
-        onChange={e => setNameInput(e.target.value)}
+        onChange={(e) => setNameInput(e.target.value)}
       />
+
+      <p className="font-bold "> Seu nome é {nameInput}</p>
+      </ExampleAreas> 
+
+      <ExampleAreas>
+        <input 
+          type="text"
+          className="border border-black p-3 text-2xl text-black rounded"
+          placeholder="Nome"
+          value={fullName.name}
+          onChange={(e) => setFullName({
+            ...fullName, name:e.target.value
+          })}
+        />
+
+        <input 
+          type="text"
+          className="border border-black p-3 text-2xl text-black rounded"
+          placeholder="Sobrenome"
+          value={fullName.lastName}
+          onChange={(e) => setFullName({
+            ...fullName, lastName: e.target.value
+          })}
+        />
+
+      <p className="font-bold "> Meu nome completo é: {fullName.name} {fullName.lastName}</p>
+      </ExampleAreas>
+         
+      {/*
 
       <button onClick={handlebutton} className="bg-blue-700 text-white p-3 rounded-md"></button>
       <p>{count2}</p>
